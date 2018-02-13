0008eebe3ba1bad90e626100a8d249312e365737
Wigouche <35629896+Wigouche@users.noreply.github.com>
2018-02-13 15:46:11 +0000

    outline program formed

--- ExampleRapid85-1753program/Program.cs
+++ ExampleRapid85-1753program/Program.cs
@@ -11,12 +11,29 @@ namespace ExampleRapid85_1753program
     {
         static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("Rapid 85-1753 Programmable power supply Example program");
+            System.Console.WriteLine("Select the COM port of the power supply");
+            //getPortName(); //get from my other project
+            //todo use result of get ports in supply contructor
+            var address = GetUserInt();
             PowerSupply supply = new PowerSupply("COM3", 1);
             supply.Connect();
+            
+            supply.output(false);
+            System.Console.WriteLine("Enter a desired output voltage: ");
+            var voltage = GetUserDec();
+            supply.SetV(voltage);
+            System.Console.WriteLine("Enter a desired output current Limit: ");
+            var current = GetUserDec();
+            System.Console.WriteLine("Get setpoints from supply.");
 
-            supply.SetUpperVoltageLimit(61);
-            Console.WriteLine( supply.GetUpperVoltageLimit());
+            System.Console.WriteLine(supply.GetSetpoints());
+            System.Console.WriteLine("press enter to turn on supply output");
+            System.Console.ReadLine();
+            supply.output(true);
+            System.Console.WriteLine("press return to get measurments from the Power supply");
+            System.Console.ReadLine();
+            System.Console.WriteLine(supply.GetMeasurments());
 
             Console.WriteLine("Paused to read results, Press return to quit.");
             Console.ReadLine();
